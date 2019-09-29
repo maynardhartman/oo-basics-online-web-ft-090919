c02be4a43539f78b5e66e643044dfe597e3b195a
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 11:35:49 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,17 @@
 class Book 
  attr_accessor :title, :author, :pages
- def @title=(title)
-    @title = title 
+ def initialize(title)
+     @title = title 
+  end
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+  end
+  def author
+    @author
   end
 end
 
