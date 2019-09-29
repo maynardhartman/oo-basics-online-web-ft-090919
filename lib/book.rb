76cd4c01eef53262c9c42b8c550d06b8e1c59d95
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 11:45:28 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,16 +1,13 @@
 class Book 
- attr_accessor :title, :author, :page_count, :genre
+ attr_accessor :author, :page_count, :genre
  def initialize(title)
      @title = title 
   end
-  def author 
-    @author = author
+  def title
+    @title 
   end
-  def page_count
-    @page_count = page_count
-  end
-  def genre
-    @genre = genre
+  def turn_page
+    puts "Flipping the page...wow, you a read fast!"
   end
 end
 
